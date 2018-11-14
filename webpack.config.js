27b4733fdd40ebc2afa08fd823b773a508f83b3b
杨卫强 <yangweiqiang@corp.netease.com>
2018-11-14 12:35:23 +0800

    add some plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    }
-  };
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    /* 没有必要再你的HTML文件中引入你的JavaScript：它会自动地注入进去。 */
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
